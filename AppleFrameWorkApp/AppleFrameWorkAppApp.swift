fd288bd81d995dbdd036e57241b3732ea0949ee6
Ritika Gupta <ritikagupta2020@gmail.com>
2025-01-02 23:13:04 +0530

    Initial Commit

---
+++ AppleFrameWorkApp/AppleFrameWorkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameWorkAppApp.swift
+//  AppleFrameWorkApp
+//
+//  Created by Ritika Gupta on 02/01/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameWorkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
